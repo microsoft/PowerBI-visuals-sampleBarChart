eadda52afd73068aba5639bf8b3b42a31dd3c4c0
Ilfat Galiev <v-ilgali@microsoft.com>
2017-05-22 17:20:53 +0300

    Adding unit test

---
+++ test/visualBuilder.ts
@@ -0,0 +1,50 @@
+/*
+ *  Power BI Visualizations
+ *
+ *  Copyright (c) Microsoft Corporation
+ *  All rights reserved.
+ *  MIT License
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the ""Software""), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+
+/// <reference path="_references.ts"/>
+
+module powerbi.extensibility.visual.test {
+    // powerbi.extensibility.utils.test
+    import VisualBuilderBase = powerbi.extensibility.utils.test.VisualBuilderBase;
+
+    // PBI_CV_9894B302_1DFF_4A96_ABFE_BF8588197166
+    import VisualClass = powerbi.extensibility.visual.PBI_CV_9894B302_1DFF_4A96_ABFE_BF8588197166.BarChart;
+
+    export class BarChartBuilder extends VisualBuilderBase<VisualClass> {
+        constructor(width: number, height: number) {
+            super(width, height);
+        }
+
+        protected build(options: VisualConstructorOptions) {
+            return new VisualClass(options);
+        }
+
+        public get mainElement() {
+            debugger;
+            return this.element.children("svg.barChart");
+        }
+    }
+}
