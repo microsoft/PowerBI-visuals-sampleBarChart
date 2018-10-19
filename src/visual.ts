46ab34c16a05b22da8e3fd6fa9e306f53cdb1da5
Konstantin Mamaev <meison@meison.net>
2018-10-19 16:05:14 +0300

    Migrate to pbiviz 3.x.x (#45)

---
+++ src/visual.ts
@@ -0,0 +1,543 @@
+import { 
+    Selection,
+    event as d3Event,
+    select as d3Select
+} from "d3-selection";
+import {
+    scaleLinear,
+    scaleBand
+} from "d3-scale";
+
+import { axisBottom } from "d3-axis";
+
+import "../style/visual.less";
+
+import { getLocalizedString } from "./localization";
+import { getValue, getCategoricalObjectValue } from "./objectEnumerationUtility";
+import { ITooltipServiceWrapper, createTooltipServiceWrapper, TooltipEventArgs } from "./tooltipServiceWrapper";
+import powerbi from "powerbi-visuals-api";
+import PrimitiveValue = powerbi.PrimitiveValue;
+import Fill = powerbi.Fill;
+import VisualObjectInstance = powerbi.VisualObjectInstance;
+import DataViewCategoryColumn = powerbi.DataViewCategoryColumn;
+import VisualObjectInstanceEnumeration = powerbi.VisualObjectInstanceEnumeration;
+import EnumerateVisualObjectInstancesOptions = powerbi.EnumerateVisualObjectInstancesOptions;
+import DataViewObjects = powerbi.DataViewObjects;
+import IVisual = powerbi.extensibility.visual.IVisual;
+import VisualTooltipDataItem = powerbi.extensibility.VisualTooltipDataItem;
+import IVisualHost = powerbi.extensibility.visual.IVisualHost;
+import VisualUpdateOptions = powerbi.extensibility.visual.VisualUpdateOptions;
+import VisualConstructorOptions = powerbi.extensibility.visual.VisualConstructorOptions;
+import ISandboxExtendedColorPalette = powerbi.extensibility.ISandboxExtendedColorPalette;
+import ISelectionId = powerbi.visuals.ISelectionId;
+import ISelectionManager = powerbi.extensibility.ISelectionManager;
+
+
+/**
+ * Interface for BarCharts viewmodel.
+ *
+ * @interface
+ * @property {BarChartDataPoint[]} dataPoints - Set of data points the visual will render.
+ * @property {number} dataMax                 - Maximum data value in the set of data points.
+ */
+interface BarChartViewModel {
+    dataPoints: BarChartDataPoint[];
+    dataMax: number;
+    settings: BarChartSettings;
+};
+
+/**
+ * Interface for BarChart data points.
+ *
+ * @interface
+ * @property {number} value             - Data value for point.
+ * @property {string} category          - Corresponding category of data value.
+ * @property {string} color             - Color corresponding to data point.
+ * @property {ISelectionId} selectionId - Id assigned to data point for cross filtering
+ *                                        and visual interaction.
+ */
+interface BarChartDataPoint {
+    value: PrimitiveValue;
+    category: string;
+    color: string;
+    strokeColor: string;
+    strokeWidth: number;
+    selectionId: ISelectionId;
+};
+
+/**
+ * Interface for BarChart settings.
+ *
+ * @interface
+ * @property {{show:boolean}} enableAxis - Object property that allows axis to be enabled.
+ * @property {{generalView.opacity:number}} Bars Opacity - Controls opacity of plotted bars, values range between 10 (almost transparent) to 100 (fully opaque, default)
+ * @property {{generalView.showHelpLink:boolean}} Show Help Button - When TRUE, the plot displays a button which launch a link to documentation.
+ */
+interface BarChartSettings {
+    enableAxis: {
+        show: boolean;
+        fill: string;
+    };
+
+    generalView: {
+        opacity: number;
+        showHelpLink: boolean;
+        helpLinkColor: string;
+    };
+}
+
+/**
+ * Function that converts queried data into a view model that will be used by the visual.
+ *
+ * @function
+ * @param {VisualUpdateOptions} options - Contains references to the size of the container
+ *                                        and the dataView which contains all the data
+ *                                        the visual had queried.
+ * @param {IVisualHost} host            - Contains references to the host which contains services
+ */
+function visualTransform(options: VisualUpdateOptions, host: IVisualHost): BarChartViewModel {
+    let dataViews = options.dataViews;
+    let defaultSettings: BarChartSettings = {
+        enableAxis: {
+            show: false,
+            fill: "#000000",
+        },
+        generalView: {
+            opacity: 100,
+            showHelpLink: false,
+            helpLinkColor: "#80B0E0",
+        }
+    };
+    let viewModel: BarChartViewModel = {
+        dataPoints: [],
+        dataMax: 0,
+        settings: <BarChartSettings>{}
+    };
+
+    if (!dataViews
+        || !dataViews[0]
+        || !dataViews[0].categorical
+        || !dataViews[0].categorical.categories
+        || !dataViews[0].categorical.categories[0].source
+        || !dataViews[0].categorical.values
+    ) {
+        return viewModel;
+    }
+
+    let categorical = dataViews[0].categorical;
+    let category = categorical.categories[0];
+    let dataValue = categorical.values[0];
+
+    let barChartDataPoints: BarChartDataPoint[] = [];
+    let dataMax: number;
+
+    let colorPalette: ISandboxExtendedColorPalette = host.colorPalette;
+    let objects = dataViews[0].metadata.objects;
+
+    const strokeColor: string = getColumnStrokeColor(colorPalette);
+
+    let barChartSettings: BarChartSettings = {
+        enableAxis: {
+            show: getValue<boolean>(objects, 'enableAxis', 'show', defaultSettings.enableAxis.show),
+            fill: getAxisTextFillColor(objects, colorPalette, defaultSettings.enableAxis.fill),
+        },
+        generalView: {
+            opacity: getValue<number>(objects, 'generalView', 'opacity', defaultSettings.generalView.opacity),
+            showHelpLink: getValue<boolean>(objects, 'generalView', 'showHelpLink', defaultSettings.generalView.showHelpLink),
+            helpLinkColor: strokeColor,
+        },
+    };
+
+    const strokeWidth: number = getColumnStrokeWidth(colorPalette.isHighContrast);
+
+    for (let i = 0, len = Math.max(category.values.length, dataValue.values.length); i < len; i++) {
+        const color: string = getColumnColorByIndex(category, i, colorPalette);
+
+        const selectionId: ISelectionId = host.createSelectionIdBuilder()
+            .withCategory(category, i)
+            .createSelectionId();
+
+        barChartDataPoints.push({
+            color,
+            strokeColor,
+            strokeWidth,
+            selectionId,
+            value: dataValue.values[i],
+            category: `${category.values[i]}`,
+        });
+    }
+
+    dataMax = <number>dataValue.maxLocal;
+
+    return {
+        dataPoints: barChartDataPoints,
+        dataMax: dataMax,
+        settings: barChartSettings,
+    };
+}
+
+function getColumnColorByIndex(
+    category: DataViewCategoryColumn,
+    index: number,
+    colorPalette: ISandboxExtendedColorPalette,
+): string {
+    if (colorPalette.isHighContrast) {
+        return colorPalette.background.value;
+    }
+
+    const defaultColor: Fill = {
+        solid: {
+            color: colorPalette.getColor(`${category.values[index]}`).value,
+        }
+    };
+
+    return getCategoricalObjectValue<Fill>(
+        category,
+        index,
+        'colorSelector',
+        'fill',
+        defaultColor
+    ).solid.color;
+}
+
+function getColumnStrokeColor(colorPalette: ISandboxExtendedColorPalette): string {
+    return colorPalette.isHighContrast
+        ? colorPalette.foreground.value
+        : null;
+}
+
+function getColumnStrokeWidth(isHighContrast: boolean): number {
+    return isHighContrast
+        ? 2
+        : 0;
+}
+
+function getAxisTextFillColor(
+    objects: DataViewObjects,
+    colorPalette: ISandboxExtendedColorPalette,
+    defaultColor: string
+): string {
+    if (colorPalette.isHighContrast) {
+        return colorPalette.foreground.value;
+    }
+
+    return getValue<Fill>(
+        objects,
+        "enableAxis",
+        "fill",
+        {
+            solid: {
+                color: defaultColor,
+            }
+        },
+    ).solid.color;
+}
+
+export class BarChart implements IVisual {
+    private svg: Selection<d3.BaseType, any, d3.BaseType, any>;
+    private host: IVisualHost;
+    private selectionManager: ISelectionManager;
+    private barContainer: Selection<d3.BaseType, any, d3.BaseType, any>;
+    private xAxis: Selection<d3.BaseType, any, d3.BaseType, any>;
+    private barDataPoints: BarChartDataPoint[];
+    private barChartSettings: BarChartSettings;
+    private tooltipServiceWrapper: ITooltipServiceWrapper;
+    private locale: string;
+    private helpLinkElement: Selection<d3.BaseType, any, d3.BaseType, any>;
+
+    private barSelection: Selection<d3.BaseType, any, d3.BaseType, any>;
+
+    static Config = {
+        xScalePadding: 0.1,
+        solidOpacity: 1,
+        transparentOpacity: 0.4,
+        margins: {
+            top: 0,
+            right: 0,
+            bottom: 25,
+            left: 30,
+        },
+        xAxisFontMultiplier: 0.04,
+    };
+
+    /**
+     * Creates instance of BarChart. This method is only called once.
+     *
+     * @constructor
+     * @param {VisualConstructorOptions} options - Contains references to the element that will
+     *                                             contain the visual and a reference to the host
+     *                                             which contains services.
+     */
+    constructor(options: VisualConstructorOptions) {
+        this.host = options.host;
+        this.selectionManager = options.host.createSelectionManager();
+
+        this.selectionManager.registerOnSelectCallback(() => {
+            this.syncSelectionState(this.barSelection, this.selectionManager.getSelectionIds() as ISelectionId[]);
+        });
+
+        this.tooltipServiceWrapper = createTooltipServiceWrapper(this.host.tooltipService, options.element);
+
+        this.svg = d3Select(options.element)
+            .append('svg')
+            .classed('barChart', true);
+
+        this.locale = options.host.locale;
+
+        this.barContainer = this.svg
+            .append('g')
+            .classed('barContainer', true);
+
+        this.xAxis = this.svg
+            .append('g')
+            .classed('xAxis', true);
+
+        const helpLinkElement: Element = this.createHelpLinkElement();
+        options.element.appendChild(helpLinkElement);
+
+        this.helpLinkElement = d3Select(helpLinkElement);
+    }
+
+    /**
+     * Updates the state of the visual. Every sequential databinding and resize will call update.
+     *
+     * @function
+     * @param {VisualUpdateOptions} options - Contains references to the size of the container
+     *                                        and the dataView which contains all the data
+     *                                        the visual had queried.
+     */
+    public update(options: VisualUpdateOptions) {
+        let viewModel: BarChartViewModel = visualTransform(options, this.host);
+        let settings = this.barChartSettings = viewModel.settings;
+        this.barDataPoints = viewModel.dataPoints;
+
+        let width = options.viewport.width;
+        let height = options.viewport.height;
+
+        this.svg
+            .attr("width", width)
+            .attr("height", height);
+
+        if (settings.enableAxis.show) {
+            let margins = BarChart.Config.margins;
+            height -= margins.bottom;
+        }
+
+        this.helpLinkElement
+            .classed("hidden", !settings.generalView.showHelpLink)
+            .style("border-color", settings.generalView.helpLinkColor)
+            .style("color", settings.generalView.helpLinkColor);
+
+        this.xAxis
+        .style("font-size", Math.min(height, width) * BarChart.Config.xAxisFontMultiplier)
+        .style("fill", settings.enableAxis.fill);
+
+        let yScale = scaleLinear()
+            .domain([0, viewModel.dataMax])
+            .range([height, 0]);
+
+        let xScale = scaleBand()
+            .domain(viewModel.dataPoints.map(d => d.category))
+            .rangeRound([0, width])
+            .padding(0.2);
+
+        let xAxis = axisBottom(xScale);
+
+        this.xAxis
+            .attr('transform', 'translate(0, ' + height + ')')
+            .call(xAxis);
+
+        this.barSelection = this.barContainer
+            .selectAll('.bar')
+            .data(this.barDataPoints);
+
+        this.barSelection
+            .enter()
+            .append('rect')
+            .classed('bar', true);
+
+        const opacity: number = viewModel.settings.generalView.opacity / 100;
+
+        this.barSelection
+            .attr("width", xScale.bandwidth())
+            .attr("height", d => height - yScale(<number>d.value))
+            .attr("y", d => yScale(<number>d.value))
+            .attr("x", d => xScale(d.category))
+            .style("fill-opacity", opacity)
+            .style("fill", d => d.color)
+            .style("stroke", d => d.strokeColor)
+            .style("stroke-width", d => `${d.strokeWidth}px`);
+
+
+        this.tooltipServiceWrapper.addTooltip(this.barContainer.selectAll('.bar'),
+            (tooltipEvent: TooltipEventArgs<BarChartDataPoint>) => this.getTooltipData(tooltipEvent.data),
+            (tooltipEvent: TooltipEventArgs<BarChartDataPoint>) => tooltipEvent.data.selectionId
+        );
+
+        this.syncSelectionState(
+            this.barSelection,
+            this.selectionManager.getSelectionIds() as ISelectionId[]
+        );
+
+        this.barSelection.on('click', (d) => {
+            // Allow selection only if the visual is rendered in a view that supports interactivity (e.g. Report)
+            if (this.host.allowInteractions) {
+                const isCtrlPressed: boolean = (d3Event as MouseEvent).ctrlKey;
+
+                this.selectionManager
+                    .select(d.selectionId, isCtrlPressed)
+                    .then((ids: ISelectionId[]) => {
+                        this.syncSelectionState(this.barSelection, ids);
+                    });
+
+                (<Event>d3Event).stopPropagation();
+            }
+        });
+
+        this.barSelection
+            .exit()
+            .remove();
+
+        // Clear selection when clicking outside a bar
+        this.svg.on('click', (d) => {
+            if (this.host.allowInteractions) {
+                this.selectionManager
+                    .clear()
+                    .then(() => {
+                        this.syncSelectionState(this.barSelection, []);
+                    });
+            }
+        });
+    }
+
+    private syncSelectionState(
+        selection: Selection<d3.BaseType, BarChartDataPoint, d3.BaseType, BarChartDataPoint>,
+        selectionIds: ISelectionId[]
+    ): void {
+        if (!selection || !selectionIds) {
+            return;
+        }
+
+        if (!selectionIds.length) {
+            selection
+                .style("fill-opacity", null)
+                .style("stroke-opacity", null);
+            return;
+        }
+
+        const self: this = this;
+
+        selection.each(function (barDataPoint: BarChartDataPoint) {
+            const isSelected: boolean = self.isSelectionIdInArray(selectionIds, barDataPoint.selectionId);
+
+            const opacity: number = isSelected
+                ? BarChart.Config.solidOpacity
+                : BarChart.Config.transparentOpacity;
+
+            d3Select(this)
+                .style("fill-opacity", opacity)
+                .style("stroke-opacity", opacity);
+        });
+    }
+
+    private isSelectionIdInArray(selectionIds: ISelectionId[], selectionId: ISelectionId): boolean {
+        if (!selectionIds || !selectionId) {
+            return false;
+        }
+
+        return selectionIds.some((currentSelectionId: ISelectionId) => {
+            return currentSelectionId.includes(selectionId);
+        });
+    }
+
+    /**
+     * Enumerates through the objects defined in the capabilities and adds the properties to the format pane
+     *
+     * @function
+     * @param {EnumerateVisualObjectInstancesOptions} options - Map of defined objects
+     */
+    public enumerateObjectInstances(options: EnumerateVisualObjectInstancesOptions): VisualObjectInstanceEnumeration {
+        let objectName = options.objectName;
+        let objectEnumeration: VisualObjectInstance[] = [];
+
+        switch (objectName) {
+            case 'enableAxis':
+                objectEnumeration.push({
+                    objectName: objectName,
+                    properties: {
+                        show: this.barChartSettings.enableAxis.show,
+                        fill: this.barChartSettings.enableAxis.fill,
+                    },
+                    selector: null
+                });
+                break;
+            case 'colorSelector':
+                for (let barDataPoint of this.barDataPoints) {
+                    objectEnumeration.push({
+                        objectName: objectName,
+                        displayName: barDataPoint.category,
+                        properties: {
+                            fill: {
+                                solid: {
+                                    color: barDataPoint.color
+                                }
+                            }
+                        },
+                        selector: barDataPoint.selectionId.getSelector()
+                    });
+                }
+                break;
+            case 'generalView':
+                objectEnumeration.push({
+                    objectName: objectName,
+                    properties: {
+                        opacity: this.barChartSettings.generalView.opacity,
+                        showHelpLink: this.barChartSettings.generalView.showHelpLink
+                    },
+                    validValues: {
+                        opacity: {
+                            numberRange: {
+                                min: 10,
+                                max: 100
+                            }
+                        }
+                    },
+                    selector: null
+                });
+                break;
+        };
+
+        return objectEnumeration;
+    }
+
+    /**
+     * Destroy runs when the visual is removed. Any cleanup that the visual needs to
+     * do should be done here.
+     *
+     * @function
+     */
+    public destroy(): void {
+        // Perform any cleanup tasks here
+    }
+
+    private getTooltipData(value: any): VisualTooltipDataItem[] {
+        let language = getLocalizedString(this.locale, "LanguageKey");
+        return [{
+            displayName: value.category,
+            value: value.value.toString(),
+            color: value.color,
+            header: language && "displayed language " + language
+        }];
+    }
+
+    private createHelpLinkElement(): Element {
+        let linkElement = document.createElement("a");
+        linkElement.textContent = "?";
+        linkElement.setAttribute("title", "Open documentation");
+        linkElement.classList.add("helpLink");
+        linkElement.addEventListener("click", () => {
+            this.host.launchUrl("https://github.com/Microsoft/PowerBI-visuals/blob/master/Readme.md#developing-your-first-powerbi-visual");
+        });
+        return linkElement;
+    };
+}
