45f286cbf91ce07992b0b6db98a82138ba3d2ffc
Sherein Dabbah <sdabbah@microsoft.com>
2022-07-05 09:40:23 +0300

    Migrate SampleBarChart to new Formatting Model Pane

--- src/barChart.ts
+++ src/barChart.ts
@@ -1,18 +1,16 @@
-import "./../style/visual.less";
+import {
+    scaleBand, scaleLinear
+} from "d3-scale";
 import {
     event as d3Event,
     select as d3Select
 } from "d3-selection";
-import {
-    scaleLinear,
-    scaleBand
-} from "d3-scale";
+import "./../style/visual.less";
 
 import { axisBottom } from "d3-axis";
 
-import powerbiVisualsApi from "powerbi-visuals-api";
+import powerbi from "powerbi-visuals-api";
 import "regenerator-runtime/runtime";
-import powerbi = powerbiVisualsApi;
 
 type Selection<T1, T2 = T1> = d3.Selection<any, T1, any, T2>;
 import ScaleLinear = d3.ScaleLinear;
@@ -21,7 +19,6 @@ const getEvent = () => require("d3-selection").event;
 // powerbi.visuals
 import DataViewCategoryColumn = powerbi.DataViewCategoryColumn;
 import DataViewObjects = powerbi.DataViewObjects;
-import EnumerateVisualObjectInstancesOptions = powerbi.EnumerateVisualObjectInstancesOptions;
 import Fill = powerbi.Fill;
 import ISandboxExtendedColorPalette = powerbi.extensibility.ISandboxExtendedColorPalette;
 import ISelectionId = powerbi.visuals.ISelectionId;
@@ -29,32 +26,16 @@ import ISelectionManager = powerbi.extensibility.ISelectionManager;
 import IVisual = powerbi.extensibility.IVisual;
 import IVisualHost = powerbi.extensibility.visual.IVisualHost;
 import PrimitiveValue = powerbi.PrimitiveValue;
-import VisualObjectInstance = powerbi.VisualObjectInstance;
-import VisualObjectInstanceEnumeration = powerbi.VisualObjectInstanceEnumeration;
 import VisualTooltipDataItem = powerbi.extensibility.VisualTooltipDataItem;
 import VisualUpdateOptions = powerbi.extensibility.visual.VisualUpdateOptions;
 import VisualConstructorOptions = powerbi.extensibility.visual.VisualConstructorOptions;
-import VisualEnumerationInstanceKinds = powerbi.VisualEnumerationInstanceKinds;
 
-import {createTooltipServiceWrapper, ITooltipServiceWrapper} from "powerbi-visuals-utils-tooltiputils";
 import { textMeasurementService } from "powerbi-visuals-utils-formattingutils";
+import { createTooltipServiceWrapper, ITooltipServiceWrapper } from "powerbi-visuals-utils-tooltiputils";
 
-import { getValue, getCategoricalObjectValue } from "./objectEnumerationUtility";
-import { getLocalizedString } from "./localization/localizationHelper"
-import { dataViewWildcard } from "powerbi-visuals-utils-dataviewutils";
-
-/**
- * Interface for BarCharts viewmodel.
- *
- * @interface
- * @property {BarChartDataPoint[]} dataPoints - Set of data points the visual will render.
- * @property {number} dataMax                 - Maximum data value in the set of data points.
- */
-interface BarChartViewModel {
-    dataPoints: BarChartDataPoint[];
-    dataMax: number;
-    settings: BarChartSettings;
-}
+import { BarChartSettingsModel } from "./barChartSettingsModel";
+import { getLocalizedString } from "./localization/localizationHelper";
+import { getCategoricalObjectValue, getValue } from "./objectEnumerationUtility";
 
 /**
  * Interface for BarChart data points.
@@ -66,7 +47,7 @@ interface BarChartViewModel {
  * @property {ISelectionId} selectionId - Id assigned to data point for cross filtering
  *                                        and visual interaction.
  */
-interface BarChartDataPoint {
+export interface BarChartDataPoint {
     value: PrimitiveValue;
     category: string;
     color: string;
@@ -75,69 +56,9 @@ interface BarChartDataPoint {
     selectionId: ISelectionId;
 }
 
-/**
- * Interface for BarChart settings.
- *
- * @interface
- * @property {{show:boolean}} enableAxis - Object property that allows axis to be enabled.
- * @property {{generalView.opacity:number}} Bars Opacity - Controls opacity of plotted bars, values range between 10 (almost transparent) to 100 (fully opaque, default)
- * @property {{generalView.showHelpLink:boolean}} Show Help Button - When TRUE, the plot displays a button which launch a link to documentation.
- */
-interface BarChartSettings {
-    enableAxis: {
-        show: boolean;
-        fill: string;
-    };
-
-    generalView: {
-        opacity: number;
-        showHelpLink: boolean;
-        helpLinkColor: string;
-    };
-
-    averageLine: {
-        show: boolean;
-        displayName: string;
-        fill: string;
-        showDataLabel: boolean;
-    };
-}
-
-let defaultSettings: BarChartSettings = {
-    enableAxis: {
-        show: false,
-        fill: "#000000",
-    },
-    generalView: {
-        opacity: 100,
-        showHelpLink: false,
-        helpLinkColor: "#80B0E0",
-    },
-    averageLine: {
-        show: false,
-        displayName: "Average Line",
-        fill: "#888888",
-        showDataLabel: false
-    }
-};
-
-/**
- * Function that converts queried data into a view model that will be used by the visual.
- *
- * @function
- * @param {VisualUpdateOptions} options - Contains references to the size of the container
- *                                        and the dataView which contains all the data
- *                                        the visual had queried.
- * @param {IVisualHost} host            - Contains references to the host which contains services
- */
-function visualTransform(options: VisualUpdateOptions, host: IVisualHost): BarChartViewModel {
+function createSelectorDataPoints(options: VisualUpdateOptions, host: IVisualHost): BarChartDataPoint[] {
+    let barChartDataPoints: BarChartDataPoint[] = []
     let dataViews = options.dataViews;
-    let viewModel: BarChartViewModel = {
-        dataPoints: [],
-        dataMax: 0,
-        settings: <BarChartSettings>{}
-    };
-
     if (!dataViews
         || !dataViews[0]
         || !dataViews[0].categorical
@@ -145,39 +66,17 @@ function visualTransform(options: VisualUpdateOptions, host: IVisualHost): BarCh
         || !dataViews[0].categorical.categories[0].source
         || !dataViews[0].categorical.values
     ) {
-        return viewModel;
+        return barChartDataPoints;
     }
 
     let categorical = dataViews[0].categorical;
     let category = categorical.categories[0];
     let dataValue = categorical.values[0];
 
-    let barChartDataPoints: BarChartDataPoint[] = [];
-    let dataMax: number;
 
     let colorPalette: ISandboxExtendedColorPalette = host.colorPalette;
-    let objects = dataViews[0].metadata.objects;
 
     const strokeColor: string = getColumnStrokeColor(colorPalette);
-
-    let barChartSettings: BarChartSettings = {
-        enableAxis: {
-            show: getValue<boolean>(objects, 'enableAxis', 'show', defaultSettings.enableAxis.show),
-            fill: getAxisTextFillColor(objects, colorPalette, defaultSettings.enableAxis.fill),
-        },
-        generalView: {
-            opacity: getValue<number>(objects, 'generalView', 'opacity', defaultSettings.generalView.opacity),
-            showHelpLink: getValue<boolean>(objects, 'generalView', 'showHelpLink', defaultSettings.generalView.showHelpLink),
-            helpLinkColor: strokeColor,
-        },
-        averageLine: {
-            show: getValue<boolean>(objects, 'averageLine', 'show', defaultSettings.averageLine.show),
-            displayName: getValue<string>(objects, 'averageLine', 'displayName', defaultSettings.averageLine.displayName),
-            fill: getValue<string>(objects, 'averageLine', 'fill', defaultSettings.averageLine.fill),
-            showDataLabel: getValue<boolean>(objects, 'averageLine', 'showDataLabel', defaultSettings.averageLine.showDataLabel),
-        },
-    };
-
     const strokeWidth: number = getColumnStrokeWidth(colorPalette.isHighContrast);
 
     for (let i = 0, len = Math.max(category.values.length, dataValue.values.length); i < len; i++) {
@@ -196,14 +95,7 @@ function visualTransform(options: VisualUpdateOptions, host: IVisualHost): BarCh
             category: `${category.values[i]}`,
         });
     }
-
-    dataMax = <number>dataValue.maxLocal;
-
-    return {
-        dataPoints: barChartDataPoints,
-        dataMax: dataMax,
-        settings: barChartSettings,
-    };
+    return barChartDataPoints;
 }
 
 function getColumnColorByIndex(
@@ -270,7 +162,7 @@ export class BarChart implements IVisual {
     private barContainer: Selection<SVGElement>;
     private xAxis: Selection<SVGElement>;
     private barDataPoints: BarChartDataPoint[];
-    private barChartSettings: BarChartSettings;
+    private formattingSettings: BarChartSettingsModel;
     private tooltipServiceWrapper: ITooltipServiceWrapper;
     private locale: string;
     private helpLinkElement: Selection<any>;
@@ -346,11 +238,10 @@ export class BarChart implements IVisual {
      *                                        the visual had queried.
      */
     public update(options: VisualUpdateOptions) {
-        let viewModel: BarChartViewModel = visualTransform(options, this.host);
-        let settings = this.barChartSettings = viewModel.settings;
-        this.barDataPoints = viewModel.dataPoints;
-        // Turn on landing page in capabilities and remove comment to turn on landing page!
-        // this.HandleLandingPage(options);
+        this.formattingSettings = BarChartSettingsModel.populateFrom(options);
+        this.barDataPoints = createSelectorDataPoints(options, this.host);
+        this.formattingSettings.populateColorSelector(this.barDataPoints);
+
         let width = options.viewport.width;
         let height = options.viewport.height;
 
@@ -358,26 +249,26 @@ export class BarChart implements IVisual {
             .attr("width", width)
             .attr("height", height);
 
-        if (settings.enableAxis.show) {
+        if (this.formattingSettings.enableAxis.show.value) {
             let margins = BarChart.Config.margins;
             height -= margins.bottom;
         }
 
         this.helpLinkElement
-            .classed("hidden", !settings.generalView.showHelpLink)
-            .style("border-color", settings.generalView.helpLinkColor)
-            .style("color", settings.generalView.helpLinkColor);
+            .classed("hidden", !this.formattingSettings.generalView.showHelpLink.value)
+            .style("border-color", this.formattingSettings.generalView.helpLinkColor)
+            .style("color", this.formattingSettings.generalView.helpLinkColor);
 
         this.xAxis
             .style("font-size", Math.min(height, width) * BarChart.Config.xAxisFontMultiplier)
-            .style("fill", settings.enableAxis.fill);
+            .style("fill", this.formattingSettings.enableAxis.fill.value);
 
         let yScale = scaleLinear()
-            .domain([0, viewModel.dataMax])
+            .domain([0, <number>options.dataViews[0].categorical.values[0].maxLocal])
             .range([height, 0]);
 
         let xScale = scaleBand()
-            .domain(viewModel.dataPoints.map(d => d.category))
+            .domain(this.barDataPoints.map(d => d.category))
             .rangeRound([0, width])
             .padding(0.2);
 
@@ -388,7 +279,7 @@ export class BarChart implements IVisual {
             .attr("color", getAxisTextFillColor(
                 colorObjects,
                 this.host.colorPalette,
-                defaultSettings.enableAxis.fill
+                this.formattingSettings.enableAxis.fill.value
             ));
 
         const textNodes = this.xAxis.selectAll("text")
@@ -406,7 +297,7 @@ export class BarChart implements IVisual {
 
         barSelectionMerged.classed('bar', true);
 
-        const opacity: number = viewModel.settings.generalView.opacity / 100;
+        const opacity: number = this.formattingSettings.generalView.opacity.value / 100;
         barSelectionMerged
             .attr("width", xScale.bandwidth())
             .attr("height", d => height - yScale(<number>d.value))
@@ -473,7 +364,7 @@ export class BarChart implements IVisual {
     }
 
     private handleContextMenu() {
-        this.svg.on('contextmenu', () => {​​
+        this.svg.on('contextmenu', () => {
             const mouseEvent: MouseEvent = getEvent();
             const eventTarget: EventTarget = mouseEvent.target;
             let dataPoint: any = d3Select(<d3.BaseType>eventTarget).datum();
@@ -494,7 +385,7 @@ export class BarChart implements IVisual {
         }
 
         if (!selectionIds.length) {
-            const opacity: number = this.barChartSettings.generalView.opacity / 100;
+            const opacity: number = this.formattingSettings.generalView.opacity.value / 100;
             selection
                 .style("fill-opacity", opacity)
                 .style("stroke-opacity", opacity);
@@ -525,89 +416,6 @@ export class BarChart implements IVisual {
             return currentSelectionId.includes(selectionId);
         });
     }
-
-    /**
-     * Enumerates through the objects defined in the capabilities and adds the properties to the format pane
-     *
-     * @function
-     * @param {EnumerateVisualObjectInstancesOptions} options - Map of defined objects
-     */
-    public enumerateObjectInstances(options: EnumerateVisualObjectInstancesOptions): VisualObjectInstanceEnumeration {
-        let objectName = options.objectName;
-        let objectEnumeration: VisualObjectInstance[] = [];
-
-        if (!this.barChartSettings ||
-            !this.barChartSettings.enableAxis ||
-            !this.barDataPoints) {
-            return objectEnumeration;
-        }
-
-        switch (objectName) {
-            case 'enableAxis':
-                objectEnumeration.push({
-                    objectName: objectName,
-                    properties: {
-                        show: this.barChartSettings.enableAxis.show,
-                        fill: this.barChartSettings.enableAxis.fill,
-                    },
-                    selector: null
-                });
-                break;
-            case 'colorSelector':
-                for (let barDataPoint of this.barDataPoints) {
-                    objectEnumeration.push({
-                        objectName: objectName,
-                        displayName: barDataPoint.category,
-                        properties: {
-                            fill: {
-                                solid: {
-                                    color: barDataPoint.color
-                                }
-                            }
-                        },
-                        propertyInstanceKind: {
-                            fill: VisualEnumerationInstanceKinds.ConstantOrRule
-                        },
-                        altConstantValueSelector: barDataPoint.selectionId.getSelector(),
-                        selector: dataViewWildcard.createDataViewWildcardSelector(dataViewWildcard.DataViewWildcardMatchingOption.InstancesAndTotals)
-                    });
-                }
-                break;
-            case 'generalView':
-                objectEnumeration.push({
-                    objectName: objectName,
-                    properties: {
-                        opacity: this.barChartSettings.generalView.opacity,
-                        showHelpLink: this.barChartSettings.generalView.showHelpLink
-                    },
-                    validValues: {
-                        opacity: {
-                            numberRange: {
-                                min: 10,
-                                max: 100
-                            }
-                        }
-                    },
-                    selector: null
-                });
-                break;
-            case 'averageLine':
-                objectEnumeration.push({
-                    objectName: objectName,
-                    properties: {
-                        show: this.barChartSettings.averageLine.show,
-                        displayName: this.barChartSettings.averageLine.displayName,
-                        fill: this.barChartSettings.averageLine.fill,
-                        showDataLabel: this.barChartSettings.averageLine.showDataLabel
-                    },
-                    selector: null
-                });
-                break;
-        };
-
-        return objectEnumeration;
-    }
-
     /**
      * Destroy runs when the visual is removed. Any cleanup that the visual needs to
      * do should be done here.
@@ -639,44 +447,6 @@ export class BarChart implements IVisual {
         return linkElement;
     };
 
-    private handleLandingPage(options: VisualUpdateOptions) {
-        if (!options.dataViews || !options.dataViews.length) {
-            if (!this.isLandingPageOn) {
-                this.isLandingPageOn = true;
-                const SampleLandingPage: Element = this.createSampleLandingPage();
-                this.element.appendChild(SampleLandingPage);
-
-                this.LandingPage = d3Select(SampleLandingPage);
-            }
-
-        } else {
-            if (this.isLandingPageOn && !this.LandingPageRemoved) {
-                this.LandingPageRemoved = true;
-                this.LandingPage.remove();
-            }
-        }
-    }
-
-    private createSampleLandingPage(): Element {
-        let div = document.createElement("div");
-
-        let header = document.createElement("h1");
-        header.textContent = "Sample Bar Chart Landing Page";
-        header.setAttribute("class", "LandingPage");
-        let p1 = document.createElement("a");
-        p1.setAttribute("class", "LandingPageHelpLink");
-        p1.textContent = "Learn more about Landing page";
-
-        p1.addEventListener("click", () => {
-            this.host.launchUrl("https://microsoft.github.io/PowerBI-visuals/docs/overview/");
-        });
-
-        div.appendChild(header);
-        div.appendChild(p1);
-
-        return div;
-    }
-
     private getColorValue(color: Fill | string): string {
         // Override color settings if in high contrast mode
         if (this.host.colorPalette.isHighContrast) {
@@ -706,13 +476,13 @@ export class BarChart implements IVisual {
     private handleAverageLineUpdate(height: number, width: number, yScale: ScaleLinear<number, number>) {
         let average = this.calculateAverage();
         let fontSize = Math.min(height, width) * BarChart.Config.xAxisFontMultiplier;
-        let chosenColor = this.getColorValue(this.barChartSettings.averageLine.fill);
+        let chosenColor = this.getColorValue(this.formattingSettings.averageLine.fill.value);
         // If there's no room to place lable above line, place it below
         let labelYOffset = fontSize * ((yScale(average) > fontSize * 1.5) ? -0.5 : 1.5);
 
         this.averageLine
             .style("font-size", fontSize)
-            .style("display", (this.barChartSettings.averageLine.show) ? "initial" : "none")
+            .style("display", (this.formattingSettings.averageLine.show.value) ? "initial" : "none")
             .attr("transform", "translate(0, " + Math.round(yScale(average)) + ")");
 
         this.averageLine.select("#averageLine")
@@ -725,7 +495,7 @@ export class BarChart implements IVisual {
         this.averageLine.select("#averageLineLabel")
             .text("Average: " + average.toFixed(2))
             .attr("transform", "translate(0, " + labelYOffset + ")")
-            .style("fill", this.barChartSettings.averageLine.showDataLabel ? chosenColor : "none");
+            .style("fill", this.formattingSettings.averageLine.showDataLabel.value ? chosenColor : "none");
     }
 
     private calculateAverage(): number {
@@ -740,5 +510,9 @@ export class BarChart implements IVisual {
         });
 
         return total / this.barDataPoints.length;
+    }
+
+    public getFormattingModel(): powerbi.visuals.FormattingModel {
+        return this.formattingSettings.buildFormattingModel();
     }
 }
---
+++ src/barChartSettingsModel.ts
@@ -0,0 +1,122 @@
+import powerbi from "powerbi-visuals-api";
+import { dataViewWildcard } from "powerbi-visuals-utils-dataviewutils";
+import { FormattingSettingsCard, FormattingSettingsModel, FormattingSettingsSlice } from "powerbi-visuals-utils-formattingmodel";
+import { BarChartDataPoint } from "./barChart";
+
+class EnableAxisCardSettings implements FormattingSettingsCard {
+    show: FormattingSettingsSlice = {
+        name: "show",
+        displayName: "Show",
+        value: false,
+        type: powerbi.visuals.FormattingComponent.ToggleSwitch
+    };
+
+    fill: FormattingSettingsSlice = {
+        name: "fill",
+        displayName: "Color",
+        value: "#000000",
+        type: powerbi.visuals.FormattingComponent.ColorPicker
+    };
+
+    name: string = "enableAxis";
+    displayName: string = "Enable Axis";
+    slices: Array<FormattingSettingsSlice> = [this.show, this.fill];
+}
+
+
+class ColorSelectorCardSettings implements FormattingSettingsCard {
+    name: string = "colorSelector";
+    displayName: string = "Data Colors";
+    slices: Array<FormattingSettingsSlice> = [];
+}
+
+class GeneralViewCardSettings implements FormattingSettingsCard {
+    opacity: FormattingSettingsSlice = {
+        name: "opacity",
+        displayName: "Bars Opacity",
+        value: 100,
+        type: powerbi.visuals.FormattingComponent.NumUpDown
+    };
+
+    showHelpLink: FormattingSettingsSlice = {
+        name: "showHelpLink",
+        displayName: "Show Help Button",
+        value: false,
+        type: powerbi.visuals.FormattingComponent.ToggleSwitch
+    };
+
+    name: string = "generalView";
+    displayName: string = "General View";
+    helpLinkColor: string = "#80B0E0"
+    slices: Array<FormattingSettingsSlice> = [this.opacity, this.showHelpLink];
+}
+
+class AverageLineCardSettings implements FormattingSettingsCard {
+    show: FormattingSettingsSlice = {
+        name: "show",
+        displayName: undefined,
+        value: false,
+        type: powerbi.visuals.FormattingComponent.ToggleSwitch
+    };
+
+    fill: FormattingSettingsSlice = {
+        name: "fill",
+        displayName: "Color",
+        value: "#888888",
+        type: powerbi.visuals.FormattingComponent.ColorPicker
+    };
+
+    showDataLabel: FormattingSettingsSlice = {
+        name: "showDataLabel",
+        displayName: "Data Label",
+        value: false,
+        type: powerbi.visuals.FormattingComponent.ToggleSwitch
+    };
+
+    name: string = "averageLine";
+    displayName: string = "Average Line";
+    analyticsPane: boolean = true;
+    slices: Array<FormattingSettingsSlice> = [this.show, this.fill, this.showDataLabel];
+}
+
+/**
+* Interface for BarChart settings.
+*
+* @interface
+* @property {{show:boolean}} enableAxis - Object property that allows axis to be enabled.
+* @property {{generalView.opacity:number}} Bars Opacity - Controls opacity of plotted bars, values range between 10 (almost transparent) to 100 (fully opaque, default)
+* @property {{generalView.showHelpLink:boolean}} Show Help Button - When TRUE, the plot displays a button which launch a link to documentation.
+*/
+export class BarChartSettingsModel extends FormattingSettingsModel {
+    enableAxis = new EnableAxisCardSettings();
+    colorSelector = new ColorSelectorCardSettings();
+    generalView = new GeneralViewCardSettings();
+    averageLine = new AverageLineCardSettings();
+
+    constructor() {
+        super();
+        this.cards = [this.enableAxis, this.colorSelector, this.generalView, this.averageLine];
+    }
+
+
+    /**
+     * populate colorSelector object categories formatting properties
+     * @param dataPoints 
+     */
+    populateColorSelector(dataPoints: BarChartDataPoint[]) {
+        let slices = this.colorSelector.slices;
+        if (dataPoints) {
+            dataPoints.forEach(dataPoint => {
+                slices.push({
+                    name: "fill",
+                    displayName: dataPoint.category,
+                    value: dataPoint.color,
+                    type: powerbi.visuals.FormattingComponent.ColorPicker,
+                    instanceKind: powerbi.VisualEnumerationInstanceKinds.ConstantOrRule,
+                    altConstantValueSelector: dataPoint.selectionId.getSelector(),
+                    selector: dataViewWildcard.createDataViewWildcardSelector(dataViewWildcard.DataViewWildcardMatchingOption.InstancesAndTotals)
+                });
+            });
+        }
+    }
+}
