f08668f642e8d317651f53ed25f5b5024e92683d
Sherein Dabbah <sdabbah@microsoft.com>
2022-10-23 09:58:13 +0300

    Description and comments modifications
    Signed-off-by: Sherein Dabbah <sdabbah@microsoft.com>

--- src/barChartSettingsModel.ts
+++ src/barChartSettingsModel.ts
@@ -4,7 +4,6 @@ import { formattingSettings } from "powerbi-visuals-utils-formattingmodel";
 import { BarChartDataPoint } from "./barChart";
 
 import Card = formattingSettings.Card;
-import Slice = formattingSettings.Slice;
 import Model = formattingSettings.Model;
 
 class EnableAxisCardSettings extends Card {
@@ -89,12 +88,7 @@ class AverageLineCardSettings extends Card {
 }
 
 /**
-* Interface for BarChart settings.
-*
-* @interface
-* @property {{show:boolean}} enableAxis - Object property that allows axis to be enabled.
-* @property {{generalView.opacity:number}} Bars Opacity - Controls opacity of plotted bars, values range between 10 (almost transparent) to 100 (fully opaque, default)
-* @property {{generalView.showHelpLink:boolean}} Show Help Button - When TRUE, the plot displays a button which launch a link to documentation.
+* BarChart formatting settings model class
 */
 export class BarChartSettingsModel extends Model {
     enableAxis = new EnableAxisCardSettings();
