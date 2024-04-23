b87db36199be7ecf80e5d4053d15a68e2e3bc11a
Iuliia Kulagina <v-ikulagina@microsoft.com>
2024-04-23 13:50:05 +0200

    Add types for settings

--- src/barChartSettingsModel.ts
+++ src/barChartSettingsModel.ts
@@ -3,26 +3,29 @@ import { BarChartDataPoint } from "./barChart";
 
 import Card = formattingSettings.SimpleCard;
 import Model = formattingSettings.Model;
+import Slice = formattingSettings.Slice;
+import ColorPicker = formattingSettings.ColorPicker;
+import ToggleSwitch = formattingSettings.ToggleSwitch;
 
 /**
  * Enable Axis Formatting Card
  */
 class EnableAxisCardSettings extends Card {
-    show = new formattingSettings.ToggleSwitch({
+    show = new ToggleSwitch({
         name: "show",
         displayName: undefined,
         value: false,
     });
 
-    fill = new formattingSettings.ColorPicker({
+    fill = new ColorPicker({
         name: "fill",
         displayName: "Color",
         value: { value: "#000000" }
     });
-    topLevelSlice = this.show;
+    topLevelSlice: ToggleSwitch = this.show;
     name: string = "enableAxis";
     displayName: string = "Enable Axis";
-    slices = [this.fill];
+    slices: Slice[] = [this.fill];
 }
 
 /**
@@ -33,7 +36,7 @@ class ColorSelectorCardSettings extends Card {
     displayName: string = "Data Colors";
 
     // slices will be populated in barChart settings model `populateColorSelector` method
-    slices = [];
+    slices: Slice[] = [];
 }
 
 /**
@@ -43,17 +46,17 @@ export class BarChartSettingsModel extends Model {
     // Create formatting settings model formatting cards
     enableAxis = new EnableAxisCardSettings();
     colorSelector = new ColorSelectorCardSettings();
-    cards = [this.enableAxis, this.colorSelector];
+    cards: Card[] = [this.enableAxis, this.colorSelector];
 
     /**
      * populate colorSelector object categories formatting properties
      * @param dataPoints 
      */
     populateColorSelector(dataPoints: BarChartDataPoint[]) {
-        const slices: formattingSettings.ColorPicker[] = this.colorSelector.slices;
+        const slices: Slice[] = this.colorSelector.slices;
         if (dataPoints) {
             dataPoints.forEach(dataPoint => {
-                slices.push(new formattingSettings.ColorPicker({
+                slices.push(new ColorPicker({
                     name: "fill",
                     displayName: dataPoint.category,
                     value: { value: dataPoint.color },
